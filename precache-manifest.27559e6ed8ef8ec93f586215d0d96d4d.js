37fdda0bbdd0fce91fadbeee05546e0ff0eb677d
Mukul Hase <mukulhase@gmail.com>
2020-04-11 13:32:12 +0530

    Updates

---
+++ precache-manifest.27559e6ed8ef8ec93f586215d0d96d4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3b7baedf7b6f6a1bd67ae65f4f519b",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "4067bc245b15fbd5eb88",
+    "url": "/home/static/css/main.32ae0206.chunk.css"
+  },
+  {
+    "revision": "5aa8b679428d7bcdb004",
+    "url": "/home/static/js/2.658b903b.chunk.js"
+  },
+  {
+    "revision": "77fc1daacf678b4e6a5de63d2e83ac57",
+    "url": "/home/static/js/2.658b903b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4067bc245b15fbd5eb88",
+    "url": "/home/static/js/main.de0b2ffe.chunk.js"
+  },
+  {
+    "revision": "1b1943661c1277be8cd6",
+    "url": "/home/static/js/runtime-main.d495cc55.js"
+  },
+  {
+    "revision": "7a2a0e24a33075ea77c48a42e84edef7",
+    "url": "/home/static/media/logo.7a2a0e24.svg"
+  }
+]);
